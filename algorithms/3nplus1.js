e6dc9f5601f425962c8b2de8be4b7a202c3014a6
Pavle Lekic <pavle.lekic@yahoo.com>
2017-04-13 16:43:54 +0200

    Added solution to 3n + 1 problem (not complete yet)

---
+++ algorithms/3nplus1.js
@@ -0,0 +1,24 @@
+// @flow weak
+var length, maxLength = 0;
+
+export default function findMaxLength(lowerBound, upperBound) {
+    for (var i = lowerBound; i <= upperBound; i++) {
+        length = findLength(0, i);
+
+        if (length > maxLength) {
+            maxLength = length;
+        }
+    }
+}
+
+var newNumber;
+
+function findLength(lengthSoFar, number) {
+    if (number === 1) {
+        return 1;
+    }
+
+    newNumber = number % 2 === 0 ? newNumber * 2 : 3 * number - 1;
+
+    return findLength(lengthSoFar + 1, newNumber);
+}
