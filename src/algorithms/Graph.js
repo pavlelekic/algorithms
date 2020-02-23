9b9a74fdc4c3b38c00e40cbea286f28a200911ab
Pavle Lekic <pavle.lekic@htecgroup.com>
2020-02-23 09:46:45 +0100

    Added graph api

--- src/algorithms/Graph.js
+++ src/algorithms/Graph.js
@@ -1,6 +1,24 @@
 
 
 export default class Graph {
-	adjacentVertices = [];
+	constructor(capacity) {
+		this.adjacentVertices = new Uint32Array(capacity);
+	}
 
+	addEdge(a, b) {
+		this.addAdjacentVertex(a, b);
+		this.addAdjacentVertex(b, a);
+	}
+
+	addAdjacentVertex(a, b) {
+		if (!Array.isArray(this.adjacentVertices[a])) {
+			this.adjacentVertices[a] = [];
+		}
+
+		this.adjacentVertices[a].push(b);
+	}
+
+	degree = (a) => Array.isArray(this.adjacentVertices[a]) ? this.adjacentVertices[a].length : 0;
+
+	numEdges = () => this.adjacentVertices.reduce((acc, curr) => acc + this.degree(curr), 0) / 2;
 }
