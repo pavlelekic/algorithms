f5e994d1da89bdaf9e176a2d859eb725b94236e0
pavle-lekic <pavle.lekic.home@gmail.com>
2020-04-05 12:57:00 +0200

    added cache

--- src/random_problems/line-of-wines.js
+++ src/random_problems/line-of-wines.js
@@ -1,15 +1,27 @@
 
 
-function maxRevenueSoFar(arr, l, r) {
-    const year = l + arr.length - r;
-
-    if (l === r) {
-        return arr[l] * year;
+export default function solution(arr) {
+    const cache = new Array(arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        cache[i] = new Array(arr.length);
     }
 
-    return Math.max(maxRevenueSoFar(arr, l+1, r) + arr[l] * year, maxRevenueSoFar(arr, l, r-1) + arr[r] * year);
-}
+    function maxRevenueSoFar(l, r) {
+        if (!cache[l][r]) {
+            const year = l + arr.length - r;
 
-export default function solution(arr) {
-    return maxRevenueSoFar(arr, 0, arr.length - 1);
+            if (l === r) {
+                return arr[l] * year;
+            }
+    
+            cache[l][r] = Math.max(
+                maxRevenueSoFar(l+1, r) + arr[l] * year,
+                maxRevenueSoFar(l, r-1) + arr[r] * year
+            );
+        }
+
+        return cache[l][r];
+    }
+
+    return maxRevenueSoFar(0, arr.length - 1);
 }
