59f210ec6176374bdedcb43070ba4f932b73d4f7
pavle-lekic <pavle.lekic.home@gmail.com>
2020-04-14 14:02:31 +0200

    vcdf

--- src/random_problems/longest-palindromic-substring.js
+++ src/random_problems/longest-palindromic-substring.js
@@ -17,16 +17,18 @@ export default function solution(str) {
     let longestPalindromicSequence = str.charAt(0);
     const cache = {};
     let l = 0;
+    let isLeftTurn = true;
     let r = str.length - 1;
-    for (let i = 0; i < str.length; i ++) {
+    while (l < r) {
         if (longestPalindromicSequence < r - l && isPalindrome(str, cache, l, r)) {
             longestPalindromicSequence = str.substring(l, r + 1);
         }
-        if (i % 2 === 0) {
+        if (isLeftTurn) {
             l++;
         } else {
             r--;
         }
+        isLeftTurn = !isLeftTurn;
     }
 
     return longestPalindromicSequence;
