e45b043b5652c4d3f86605140dd1f59b140376de
pavle-lekic <pavle.lekic.home@gmail.com>
2020-04-18 17:09:26 +0200

    more tests

--- src/random_problems/max-subarray-sum.test.js
+++ src/random_problems/max-subarray-sum.test.js
@@ -3,3 +3,7 @@ import solution from './max-subarray-sum';
 test('Lalaallaa', () => {
     expect(solution([-1, 2, 4, -3, 5, 2, -5, 2])).toEqual(10);
 });
+
+test('Lalaallaa2', () => {
+    expect(solution([-2,1,-3,4,-1,2,1,-5,4])).toEqual(6);
+});
