20aa601e4031536ef03b48a9ff0f2291799b839b
pavle-lekic <pavle.lekic.home@gmail.com>
2020-05-02 19:09:48 +0200

    added path

--- src/random_problems/coins-iterative.js
+++ src/random_problems/coins-iterative.js
@@ -2,13 +2,15 @@
 
 export default function(coins, amount) {
     const dp = new Array(amount + 1);
+    // const usedCoin = new Array(amount + 1);
     dp[0] = 0;
 
     for (let i = 1; i <= amount; i++) {
         dp[i] = Number.POSITIVE_INFINITY;
         coins.forEach(c => {
-            if (typeof dp[i - c] !== 'undefined') {
-                dp[i] = Math.min(dp[i], dp[i - c]  + 1);
+            if (typeof dp[i - c] !== 'undefined' && dp[i] > dp[i - c]  + 1) {
+                dp[i] = dp[i - c]  + 1;
+                // usedCoin[i] = c;
             }
         });
     }
