93279481bae5d48771b476b345992a116f7dac0d
pavle-lekic <pavle.lekic.home@gmail.com>
2020-05-10 12:38:43 +0200

    anoyher

---
+++ src/leetcode/max-sum-divisible-by-3.js
@@ -0,0 +1,16 @@
+
+export default function(nums) {
+    const currentMaxes = [0, 0, 0];
+    let prevMaxes;
+
+    nums.forEach(n => {
+        prevMaxes = currentMaxes.slice();
+        let potentialNewMax;
+        prevMaxes.forEach(prevMax => {
+            potentialNewMax = n + prevMax;
+            currentMaxes[potentialNewMax % 3] = Math.max(currentMaxes[potentialNewMax % 3], potentialNewMax);
+        });
+    });
+
+    return currentMaxes[0];
+}
